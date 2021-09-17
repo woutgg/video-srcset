5b7e6450fc78e41f607c487627e20f9c88b83084
Wouter R <gennerate@zonnet.nl>
2021-09-17 15:55:20 +0200

    Allow specifying media sizes using data-sizes attribute.

--- src/video-srcset.js
+++ src/video-srcset.js
@@ -62,19 +62,51 @@
 		}
 
 		/**
-		 * @param srcsrt The definition of the srcset attribute
-		 * @param screenWidth The width of the container to find matching src for
+		 * @param string|undefined spec The sizes specification (format: 'WxH, WxH, ...')
+		 * @returns Array<{width:number, height:number}>
+		 */
+		function parseMediaSizes(spec) {
+			if (typeof spec === 'string' || spec instanceof String) {
+				var sizes = [];
+				var parts = spec.split(',');
+
+				for (var i = 0; i < parts.length; i++) {
+					var nums = parts[i].split('x');
+					sizes.push({
+						width: parseInt(nums[0]),
+						height: parseInt(nums[1]),
+					});
+				}
+
+				return sizes;
+			}
+
+			return [];
+		}
+
+		/**
+		 * @param string srcsrt The definition of the srcset attribute
+		 * @param string mediaSizes The definition of media sizes
+		 * @param number screenWidth The width of the container to find matching src for
+		 * @param number screenRatio The size ratio of the container to find matching src for
 		 * @returns string The best matching video source
 		 */
-		function selectSource(srcsrt, screenWidth, screenRatio) {
+		function selectSource(srcsrt, mediaSizes, screenWidth, screenRatio) {
 			var sources = getSourceSets(srcsrt);
 			var hasSizes = false, hasRatios = false;
+			var mediaSizeArr = parseMediaSizes(mediaSizes);
 			var defaultSource = null;
 
 			for (var i = 0; i < sources.length; i++) {
 				hasSizes |= sources[i].width !== undefined;
 				hasRatios |= sources[i].ratio !== undefined;
-				if (sources[i].isDefault) defaultSource = sources[i].src;
+
+				if (sources[i].isDefault) defaultSource = sources[i];
+
+				if (Array.isArray(mediaSizeArr) && mediaSizeArr.length > i) {
+					sources[i].mediaWidth = mediaSizeArr[i].width;
+					sources[i].mediaHeight = mediaSizeArr[i].height;
+				}
 			}
 
 			if (hasSizes && hasRatios) {
@@ -105,7 +137,7 @@
 						(candidateDiff >= 0 && candidateDiff < selectedDiff ) // Got one that match better
 				) {
 					selectedDiff = candidateDiff;
-					source = candidate.src;
+					source = candidate;
 				}
 			}
 
@@ -126,7 +158,7 @@
 						(candidateDiff >= 0 && candidateDiff < selectedDiff ) // Got one that match better
 				) {
 					selectedDiff = candidateDiff;
-					source = candidate.src;
+					source = candidate;
 				}
 			}
 
@@ -137,17 +169,20 @@
 			// Select sources for valid elements from the requested ones
 			for (var i = 0; i < elements.length; i++) {
 				var element = elements[i];
-				// If the element isn't a <video> tag with srcset="..." attribute - don't even check it
-				if (element.tagName == 'VIDEO' && element.hasAttribute('srcset')) {
-					var srcset = element.getAttribute('srcset');
+				// If the element isn't a <video> tag with data-srcset="..." attribute - don't even check it
+				if (element.tagName == 'VIDEO' && element.hasAttribute('data-srcset')) {
+					var srcset = element.getAttribute('data-srcset');
+					var sizes = element.getAttribute('data-sizes');
 
 					// check if srcset is not empty
 					if(srcset) {
 						var ratio = window.innerWidth / window.innerHeight;
-						var selectedSource = selectSource(srcset, window.innerWidth, ratio);
+						var selectedSource = selectSource(srcset, sizes, window.innerWidth, ratio);
 						// Don't reapply the same src (to prevent reloading of the same video if run in resize, etc...)
-						if(selectedSource !== element.src) {
-							element.src = selectedSource;
+						if (selectedSource.src !== element.src) {
+							element.src = selectedSource.src;
+							if ('mediaWidth' in selectedSource) element.width = selectedSource.mediaWidth;
+							if ('mediaHeight' in selectedSource) element.height = selectedSource.mediaHeight;
 						}
 					}
 				}
